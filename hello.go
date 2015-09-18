c90b21106522ec22d7307596e38bc7f3f8efd057
YongTak Kim <ytkim4558@naver.com>
2015-09-18 20:22:51 +0900

    hello.py modified on Local repository

--- hello.go
+++ hello.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main() {
+	// For command line git tutorial
+	// Local repository modification
+
 	fmt.Println("Hello, world!")
 	fmt.Println("Tell Your World")
 	fmt.Println("Tell his world")
