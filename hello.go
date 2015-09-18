9e1eeda91f691daeeb87b5f0caf8b03d5a601025
ytkim4558 <ytkim4558@naver.com>
2015-09-18 20:19:29 +0900

    hello.go modified on GitHub

--- hello.go
+++ hello.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main() {
+	// For command line git tutorial
+	// Github modification
+	
 	fmt.Println("Hello, world!")
 	fmt.Println("Tell Your World")
 	fmt.Println("Tell his world")
